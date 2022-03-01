24c0ffd24a27ca4a30faf85949dc9caa051c8195
Ryan Gehrlein <ryan.gehrlein@gmail.com>
2022-03-01 18:02:40 -0500

    Spotlight detection done p much

---
+++ GAME3400 TEAM 5 PROJECT 5/Assets/Scripts/SpotlightLineOfSight.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(Light))]
+public class SpotlightLineOfSight : MonoBehaviour
+{
+    public static event Action<GameObject> PlayerIsInLight;
+
+    [SerializeField]
+    private string playerTag = "Player";
+
+    private Light light;
+    private GameObject player;
+
+    void Start()
+    {
+        this.light = this.GetComponent<Light>();
+        this.light.type = LightType.Spot;
+
+        this.player = GameObject.FindGameObjectWithTag(this.playerTag);
+    }
+
+    void Update()
+    {
+        if (this.IsPlayerInLight())
+        {
+            this.BroadcastPlayerSpotted();
+        }
+    }
+
+    private float LightHalfAngle()
+    {
+        return this.light.spotAngle / 2;
+    }
+
+    private Vector3 ToPlayer()
+    {
+        return this.player.transform.position - this.transform.position;
+    }
+
+    private bool IsPlayerInLight()
+    {
+        return this.IsPlayerInCone() && this.IsPlayerInLineOfSight();
+    }
+
+    private bool IsPlayerInCone()
+    {
+        float distanceToPlayer = this.ToPlayer().magnitude;
+        float angleToPlayer = Vector3.Angle(this.transform.forward, this.ToPlayer());
+        return angleToPlayer <= this.LightHalfAngle() && distanceToPlayer <= this.light.range;
+    }
+
+    private bool IsPlayerInLineOfSight()
+    {
+        Ray ray = new Ray(this.transform.position, this.ToPlayer());
+        if (Physics.Raycast(this.transform.position, this.ToPlayer(), out RaycastHit hit, this.light.range, ~LayerMask.GetMask()))
+        {
+            return hit.collider.CompareTag(this.playerTag);
+        }
+        return false;
+    }
+
+    private void BroadcastPlayerSpotted()
+    {
+        Debug.Log("SPOTTED");
+        if (PlayerIsInLight != null)
+        {
+            PlayerIsInLight.Invoke(this.gameObject);
+        }
+    }
+}
