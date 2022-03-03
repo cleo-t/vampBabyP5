c42d1c84f9af767253638377db9705d6125b807e
Ryan Gehrlein <ryan.gehrlein@gmail.com>
2022-03-02 22:57:07 -0500

    Door changes

---
+++ GAME3400 TEAM 5 PROJECT 5/Assets/Scripts/MoveDoorOnSwitch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDoorOnSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private float arcDegrees = 90;
+    [SerializeField]
+    private float moveDuration = 0.75f;
+
+    void Start()
+    {
+        DoorSwitchCollider.OnSwitchDoors += this.OnDoorSwitch;
+    }
+
+    private void OnDoorSwitch()
+    {
+        Debug.Log("ACKNOWLEDGED!");
+        StartCoroutine(this.MoveDoor());
+    }
+
+    private IEnumerator MoveDoor()
+    {
+        float degreesMoved = 0;
+        float degreesPerSecond = this.arcDegrees / this.moveDuration;
+        while(Mathf.Abs(degreesMoved) < Mathf.Abs(this.arcDegrees))
+        {
+            float step = degreesPerSecond * Time.deltaTime;
+            this.transform.localRotation *= Quaternion.AngleAxis(step, Vector3.up);
+            degreesMoved += step;
+            yield return null;
+        }
+        this.transform.localRotation *= Quaternion.AngleAxis(this.arcDegrees - degreesMoved, Vector3.up);
+    }
+}
